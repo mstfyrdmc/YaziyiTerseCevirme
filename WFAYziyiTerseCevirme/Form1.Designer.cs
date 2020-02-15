4160c0e28460149e79009ea385872617baf8f701
mstfyrdmc <45285832+AdamGiby@users.noreply.github.com>
2020-02-15 19:20:13 +0300

    YaziyiTersCevir

---
+++ WFAYziyiTerseCevirme/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WFAYziyiTerseCevirme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMetin = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCevir = new System.Windows.Forms.Button();
+            this.lblGoster = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtMetin
+            // 
+            this.txtMetin.Location = new System.Drawing.Point(12, 32);
+            this.txtMetin.Name = "txtMetin";
+            this.txtMetin.Size = new System.Drawing.Size(226, 20);
+            this.txtMetin.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Metni Giriniz:";
+            // 
+            // btnCevir
+            // 
+            this.btnCevir.Location = new System.Drawing.Point(12, 58);
+            this.btnCevir.Name = "btnCevir";
+            this.btnCevir.Size = new System.Drawing.Size(226, 23);
+            this.btnCevir.TabIndex = 2;
+            this.btnCevir.Text = " Çevir";
+            this.btnCevir.UseVisualStyleBackColor = true;
+            this.btnCevir.Click += new System.EventHandler(this.btnCevir_Click);
+            // 
+            // lblGoster
+            // 
+            this.lblGoster.Location = new System.Drawing.Point(12, 93);
+            this.lblGoster.Name = "lblGoster";
+            this.lblGoster.Size = new System.Drawing.Size(226, 23);
+            this.lblGoster.TabIndex = 3;
+            this.lblGoster.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 162);
+            this.Controls.Add(this.lblGoster);
+            this.Controls.Add(this.btnCevir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMetin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMetin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCevir;
+        private System.Windows.Forms.Label lblGoster;
+    }
+}
+
---
+++ WFAYziyiTerseCevirme/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAYziyiTerseCevirme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCevir_Click(object sender, EventArgs e)
+        {
+            lblGoster.Text = null;
+            if (!String.IsNullOrEmpty(txtMetin.Text))
+            {
+                string metin = txtMetin.Text;
+
+                for (int i = metin.Length - 1; i >= 0; i--)
+                {
+
+                    lblGoster.Text += metin[i];
+                }
+            }
+            else {
+                lblGoster.Text = "Lütfen Boş bırakmayınız!";
+                
+            }
+            //lblGoster.Text = null;
+        }
+    }
+}
